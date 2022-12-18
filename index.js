6e815e2927784f47d5b4875bfb599234c464f24f
vikram rathode <vikramrambhad25@gmail.com>
2022-12-18 13:45:42 +0530

    push

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
-        // url: 'https://backend-airbnb-clone.vercel.app'
+        // url: "http://localhost:3000",
+        url: 'https://backend-airbnb-clone.vercel.app'
       },
     ],
   },
